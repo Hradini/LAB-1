03846a4b7d3a39415ef22c1cbc2f1517923228c3
Hradini <31266306+Hradini@users.noreply.github.com>
2017-08-24 17:27:16 +0530

    Create Modulooperation.cpp

---
+++ Modulooperation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+	int num1, num2;
+	printf("Enter first number and second number \n");
+	scanf ("%d" , &num1);
+	scanf ("%d" , &num2);
+ 
+	printf("%d" , num1 % num2 );
+	return 0;
+}
